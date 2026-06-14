8af86044c57598c57018767d527647e1fb468829
Melvin Walls <melvin@melvinw.com>
2026-06-14 19:15:53 -0400

    fit: integrate generated profile code
    For now, just import and print something from it to make sure it builds.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
 const gnsslib = @import("gnsslib");
+const profile = @import("gen/profile.zig");
 
-pub fn main(_: std.process.Init) !void {}
+pub fn main(_: std.process.Init) !void {
+    std.debug.print("{}\n", .{profile.MesgNum.activity});
+}
